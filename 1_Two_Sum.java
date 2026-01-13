6d83c9010ddcbf256b3333610593be2bd335335c
GAURAV BHUVA <161735757+gauravbhuva@users.noreply.github.com>
2026-01-13 17:50:55 +0530

    Return indices of sum elements

---
+++ 1_Two_Sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int index[]=new int[2];
+        for (int i = 0; i <nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                int sum = nums[i]+nums[j];
+                if (target == sum) {
+                    index[0]=i;
+                    index[1]=j;
+                }
+            }
+        }
+        return index;
+    }
+}
