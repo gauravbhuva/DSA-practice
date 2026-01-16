04e1fb67558cd05501705adde9b248ac207741ea
GAURAV BHUVA <161735757+gauravbhuva@users.noreply.github.com>
2026-01-16 16:43:42 +0530

    Optimize maxProfit method for better performance
    Refactored maxProfit method to improve time complexity by using a single loop and tracking the minimum price.

--- 121_Best_Time_to_Buy_and_Sell_Stock.java
+++ 121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -1,15 +1,34 @@
+// FIRST SOLUTION THAT CAME IN MIND BUT IT HAS LARGE TIME COMPLEXITY
+// class Solution {
+//     public int maxProfit(int[] prices) {
+//         int i,j;
+//         int profit =0;
+//         int maxp=0;
+//         for(i=prices.length-1 ;i>0;i--){
+//             for(j=i-1;j>=0;j--){
+//                 profit = prices[i]-prices[j];
+//                 if(profit>maxp){
+//                     maxp=profit;
+//                 }
+//         }
+//         }
+//         return maxp;
+//     }
+// }
+
+
 class Solution {
     public int maxProfit(int[] prices) {
-        int i,j;
-        int profit =0;
-        int maxp=0;
-        for(i=prices.length-1 ;i>0;i--){
-            for(j=i-1;j>=0;j--){
-                profit = prices[i]-prices[j];
-                if(profit>maxp){
-                    maxp=profit;
-                }
-        }
+        int buy = prices[0];
+        int profit , maxp=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<buy){
+                buy=prices[i];
+            }
+            profit =prices[i]-buy;
+            if(profit>maxp){
+                maxp=profit;
+            }
         }
         return maxp;
     }
