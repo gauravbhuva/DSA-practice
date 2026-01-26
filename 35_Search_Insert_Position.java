f9c0528050e2f55129f73fcb8a0c80f6d05b13d2
GAURAV BHUVA <161735757+gauravbhuva@users.noreply.github.com>
2026-01-26 21:23:13 +0530

    Return possible index of target or the current one

---
+++ 35_Search_Insert_Position.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }
+            if(nums[i]<target){
+                k++;
+            }
+        }
+        return k;
+    }
+}
